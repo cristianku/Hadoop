ac16e73db301db4d65ddc86876881394478e8d5a
cristianku <c.zantedeschi@gmail.com>
2017-05-26 19:26:51 +0200

    commit

---
+++ CustomerOrders.py
@@ -0,0 +1,15 @@
+from mrjob.job import MRJob
+
+class MRCustomerOrders(MRJob):
+    def mapper(self, key, line):
+        (customer, item, order_amount) = line.split(',')
+        yield '%04d'%int(customer), float(order_amount)
+
+    def reducer(self, key, values_list):
+        yield key, sum(values_list)
+
+if __name__ == '__main__':
+    # please put in the script parameter ( python command line ) : ml-100k/u.data
+    #example : python customer-orders.csv
+
+    MRCustomerOrders.run()
