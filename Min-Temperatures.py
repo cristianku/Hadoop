2d768c4fa3ab32d0844046a20997b78e3a8a21e7
cristianku <c.zantedeschi@gmail.com>
2017-05-26 18:42:03 +0200

    commit

--- Min-Temperatures.py
+++ Min-Temperatures.py
@@ -7,10 +7,15 @@ class MRMinTemperature(MRJob):
         fahrenheit = celsius * 1.8 + 32.0
         return fahrenheit
 
+
+    def MakeCelsius(self, tenthsOfCelsius):
+        celsius = float(tenthsOfCelsius) / 10.0
+        return celsius
+
     def mapper(self, _, line):
-        (location, date, type, data, x, y, z, w) = line.split(',')
+        (location, date, type, temp, x, y, z, w) = line.split(',')
         if (type == 'TMIN'):
-            temperature = self.MakeFahrenheit(data)
+            temperature = self.MakeCelsius(temp)
             yield location, temperature
 
     def reducer(self, location, temps):
@@ -19,3 +24,7 @@ class MRMinTemperature(MRJob):
 
 if __name__ == '__main__':
     MRMinTemperature.run()
+
+
+# example:
+# python Min-Temperatures.py data/1800.csv
